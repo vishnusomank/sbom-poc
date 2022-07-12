c250648a19dff046b237cc9aa8bdc55003642ce7
vishnu <vishnu@accuknox.com>
2022-07-12 19:27:56 +0530

    Updated db file to include Grype vulnerabilities

--- models/db.go
+++ models/db.go
@@ -1,8 +1,9 @@
 package models
 
 type SBOM struct {
-	ID        uint   `json:"id" gorm:"primary_key"`
-	ImageName string `json:"image"`
-	Version   string `json:"version"`
-	Value     string `json:"value"`
+	ID            uint   `json:"id" gorm:"primary_key"`
+	ImageName     string `json:"image"`
+	Version       string `json:"version"`
+	Value         string `json:"sbom"`
+	Vulnerability string `json:"vulnerability"`
 }
