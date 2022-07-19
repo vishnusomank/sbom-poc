ea9cf35abbe72a8bbe28e4ea1c94d5fadaa2db10
vishnusomank <vishnu@accuknox.com>
2022-07-20 00:40:38 +0530

    updated grype file to include db search logic

--- src/grype/grype.go
+++ src/grype/grype.go
@@ -1,13 +1,21 @@
 package grype
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/tidwall/gjson"
+	"github.com/vishnusomank/sbom-poc/models"
 )
 
-func StartGrype(imageName, version string) []byte {
+func StartGrype(imageName, version string, id int, sbom *models.SBOM) {
 
 	cmd := exec.Command("/bin/sh", "-c", "grype sbom:./"+imageName+"_"+version+".json  -o json > grype-"+imageName+"_"+version+".json")
 	_, err := cmd.Output()
@@ -18,17 +26,54 @@ func StartGrype(imageName, version string) []byte {
 
 	if err != nil {
 		log.Fatal(err)
-		return nil
 	}
 
 	defer fileContent.Close()
 
 	byteResult, _ := ioutil.ReadAll(fileContent)
 
+	if err := models.DB.Model(&sbom).Where("id = ?", id).Update("Vulnerability", string(byteResult)).Error; err != nil {
+		fmt.Printf("[%s][%s] Unable to update SBOM value\n", color.BlueString(time.Now().Format("01-02-2006 15:04:05")), color.RedString("ERR"))
+
+		return
+	}
+
+	var policydb []models.PolicyDB
+	count := models.POLICYDB.Find(&policydb)
+
+	file, _ := ioutil.ReadFile("grype-" + imageName + "_" + version + ".json")
+	value := gjson.Get(string(file), "matches.#")
+	numValue, _ := strconv.Atoi(value.Raw)
+
+	fmt.Println("rows affected ", count.RowsAffected)
+
+	for i := 1; i <= numValue; i++ {
+		polcount := 0
+		dataVal := gjson.Get(string(file), "matches."+strconv.Itoa(i)+".vulnerability.id")
+		println("data value", dataVal.String())
+		for j := 1; j <= int(count.RowsAffected); j++ {
+			if err := models.POLICYDB.Where("ID = ?", j).First(&policydb).Error; err != nil {
+				fmt.Printf("[%s][%s] Failed to retrieve data %v\n", color.RedString(time.Now().Format("01-02-2006 15:04:05")), color.RedString("ERR"), err)
+				return
+			}
+			fmt.Println("polcount outside ", polcount)
+			if strings.Contains(policydb[0].CVEId, dataVal.String()) && polcount == 0 {
+				fmt.Println("polcount inside ", polcount)
+				fmt.Println("contains ", policydb[0].CVEId)
+				sbompolicy := models.SBOMPolicy{SbomID: id, PolicyID: j}
+				models.SBOMPOLICYDB.Create(&sbompolicy)
+				polcount = 1
+				fmt.Println("polcount inside after change ", polcount)
+			}
+
+		}
+
+	}
+
 	cmd = exec.Command("/bin/sh", "-c", "rm "+imageName+"_"+version+".json"+" grype-"+imageName+"_"+version+".json")
 	_, err = cmd.Output()
 	if err != nil {
-		log.Fatal("SBOM generation failed : " + err.Error())
+		log.Fatal("File deletion failed : " + err.Error())
 	}
-	return byteResult
+
 }
